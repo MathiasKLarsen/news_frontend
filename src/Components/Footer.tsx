60b9039b21a0294f85085fcaca15fcf80f624818
MathiasKLarsen <mathiaskrogkaer@gmail.com>
2025-10-30 00:14:36 +0100

    footer and some extra styling

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,19 +1,62 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className='flex flex-col py-2 bg-black text-white'>
-      <section className='grid grid-cols-4 h-50 text-center items-center gap-5'>
-        <div>footer</div>
-        <div>footer</div>
-        <div>footer</div>
-        <div>footer</div>
-      </section>
-      <section className='flex py-5 justify-center border-t-2'>
-          <p>
-            &copy; | TLF: 12 34 56 78
+    <footer className="flex flex-col py-2 bg-black text-white">
+      <section className="max-w-[1000px] px-5 md:px-0 md:gap-x-26 gap-y-10 mx-auto mb-10 flex justify-between flex-wrap">
+        {/* First Column */}
+        <div className="font-semibold">
+          <h3 className="text-2xl mb-4">Nyheder</h3>
+          <ul>
+            <li>Seneste nyt</li>
+            <li>Internationalt</li>
+            <li>Sport</li>
+            <li>Vejret</li>
+            <Link href="/contact" className="hover:text-gray-400 transition">
+              <li>Contact</li>
+            </Link>
+          </ul>
+        </div>
+
+        {/* Second Column */}
+        <div className="font-semibold">
+          <h3 className="text-2xl font-semibold mb-4">Lorem, ipsum dolor.</h3>
+          <p className="w-50">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum
+            corporis voluptates quas sit non, qui et consequuntur? Molestiae,
+            tempore minus?
           </p>
+        </div>
+
+        {/* Third Column */}
+        <div className="font-semibold">
+          <h3 className="text-2xl font-semibold mb-4">Lorem, ipsum dolor.</h3>
+          <p className="w-50">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum
+            corporis voluptates quas sit non, qui et consequuntur? Molestiae,
+            tempore minus?
+          </p>
+        </div>
+
+        {/* Fourth Column */}
+        <div className="font-semibold">
+          <h3 className="text-2xl font-semibold mb-4">Om NEWS</h3>
+          <ul>
+            <li>Nyt fra NEWS</li>
+            <li>Job i NEWS</li>
+            <li>Presse</li>
+            <li>Vilkår på NEWS</li>
+            <li>Etik og rettelser</li>
+            <li>Privatlivspolitik</li>
+          </ul>
+        </div>
+        
+      </section>
+      <section className="flex py-5 justify-center border-t-2">
+        <p>copyright &copy; 2025 | TLF: 12 34 56 78</p>
       </section>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/vejr/WeatherCard.tsx
+++ src/Components/vejr/WeatherCard.tsx
@@ -4,18 +4,19 @@ import { Sun, Cloud, CloudRain } from "lucide-react";
 const WeatherCard = async () => {
   const weatherData = await openWeather();
 
-  const getIcon = (main: string) => {
-    if (main.includes("Rain")) return <CloudRain size={40} />;
-    if (main.includes("Cloud")) return <Cloud size={40} />;
-    return <Sun size={40} />;
+  const getIcon = (main: string, size: number) => {
+    if (main.includes("Rain")) return <CloudRain size={size} />;
+    if (main.includes("Cloud")) return <Cloud size={size} />;
+    return <Sun size={size} />;
   };
 
   return (
     <section className="grid md:grid-cols-3 gap-x-5 text-white">
       {/* Venstre del */}
-      <div className="md:col-span-2 grid grid-rows-[1fr_auto_auto] gap-y-5">
-        <div className="p-6 flex flex-col justify-between cyan">
-          <div>
+      <div className="grid md:col-span-2 grid-rows-[1fr_auto_auto] gap-y-5">
+        <div className="p-6 grid grid-cols-2 cyan">
+          {/* overskrift */}
+          <div className="flex justify-between">
             <h2 className="text-lg font-semibold">{weatherData.city.name}</h2>
             <p className="text-sm opacity-80 mb-4">
               {new Date().toLocaleDateString("da-DK", {
@@ -27,19 +28,25 @@ const WeatherCard = async () => {
             </p>
           </div>
 
-          <div className="flex justify-between items-center">
+          {/* icon */}
+          <div className="flex justify-center items-center">
+            {getIcon(weatherData.list[0].weather[0].main, 120)}
+          </div>
+
+          {/* grader */}
+          <div className="flex justify-center items-center">
             <div>
-              <h1 className="text-8xl font-bold leading-none">
+              <h3 className="text-8xl font-bold leading-none">
                 {Math.round(weatherData.list[0].main.temp)}°
-              </h1>
+              </h3>
               <p className="capitalize text-lg">
                 {weatherData.list[0].weather[0].description}
               </p>
             </div>
-            {getIcon(weatherData.list[0].weather[0].main)}
           </div>
 
-          <div className="flex justify-between text-sm mt-6">
+          {/* samlet temp */}
+          <div className="flex justify-between items-end text-sm mt-6">
             {weatherData.list.slice(0, 4).map((f: any, i) => (
               <p key={i}>
                 {i === 0
@@ -56,74 +63,88 @@ const WeatherCard = async () => {
         </div>
 
         {/* Vind + regn */}
-        <div className="grid grid-cols-2 gap-5 text-center text-sm">
-          <div className="cyan p-4">
-            <p>Vind</p>
-            <p className="text-2xl font-bold">
-              {weatherData.list[0].wind.speed}
-            </p>
-            <p className="text-xs">m/s</p>
+        <section className="grid grid-cols-2 gap-5 text-sm">
+          <div className="cyan p-4 flex flex-col gap-y-4 justify-between">
+            <p className="text-sm">Vind</p>
+            <div className="flex justify-between items-end">
+              <p className="text-xs">m/s</p>
+              <p className="text-2xl font-bold">
+                {weatherData.list[0].wind.speed}
+              </p>
+            </div>
           </div>
-          <div className="cyan p-4">
-            <p>Chance for regn</p>
-            <p className="text-2xl font-bold">
-              {weatherData.list[0]?.pop
-                ? Math.round(weatherData.list[0].pop * 100)
-                : 0}
-              %
-            </p>
+
+          <div className="cyan p-4 flex flex-col gap-y-4 justify-between">
+            <p className="text-sm">Chance for regn</p>
+            <div className="flex justify-between items-end">
+              <p className="text-xs">%</p>
+              <p className="text-2xl font-bold">
+                {weatherData.list[0]?.pop
+                  ? Math.round(weatherData.list[0].pop * 100)
+                  : 0}
+              </p>
+            </div>
           </div>
-        </div>
+        </section>
 
         {/* Luftfugtighed + UV (fake UV pga) */}
-        <div className="grid grid-cols-2 gap-5 text-center text-sm">
-          <div className="cyan p-4">
-            <p>Luftfugtighed</p>
-            <p className="text-2xl font-bold">
-              {weatherData.list[0].main.humidity}
-            </p>
-            <p className="text-xs">%</p>
+        <section className="grid grid-cols-2 gap-5 text-sm">
+          {/* First block */}
+          <div className="cyan p-4 flex flex-col gap-y-4 justify-between">
+            <p className="text-sm">Luftfugtighed</p>
+            <div className="flex justify-between items-end">
+              <p className="text-xs">%</p>
+              <p className="text-2xl font-bold">
+                {weatherData.list[0].main.humidity}
+              </p>
+            </div>
           </div>
-          <div className="cyan p-4">
-            <p>UV Indeks</p>
-            <p className="text-2xl font-bold">0.5</p>
+
+          {/* Second Block */}
+          <div className="cyan p-4 flex flex-col gap-y-4 justify-between">
+            <p className="text-sm">UV Indeks</p>
+            <div className="flex justify-end items-end">
+              <p className="text-2xl font-bold">0.5</p>
+            </div>
           </div>
-        </div>
+        </section>
       </div>
 
       {/* Prognose */}
       <div className="flex flex-col justify-evenly cyan p-4">
-        {weatherData.list.filter((x, i) => i %8 == 0).map((d: any, i: number) => (
-          <div
-            key={i}
-            className="flex justify-between items-center border-b py-1 text-sm"
-          >
-            <div>
-              <p className="font-semibold">
-                {i === 0
-                  ? "I dag"
-                  : new Date(d.dt * 1000).toLocaleDateString("da-DK", {
-                      weekday: "long",
-                    })}
-              </p>
-              <p className="text-xs opacity-80">
-                {new Date(d.dt * 1000).toLocaleDateString("da-DK", {
-                  day: "numeric",
-                  month: "long",
-                })}
-              </p>
+        {weatherData.list
+          .filter((x, i) => i % 8 == 0)
+          .map((d: any, i: number) => (
+            <div
+              key={i}
+              className="flex justify-between items-center border-b py-1 text-sm"
+            >
+              <div>
+                <p className="font-semibold">
+                  {i === 0
+                    ? "I dag"
+                    : new Date(d.dt * 1000).toLocaleDateString("da-DK", {
+                        weekday: "long",
+                      })}
+                </p>
+                <p className="text-xs opacity-80">
+                  {new Date(d.dt * 1000).toLocaleDateString("da-DK", {
+                    day: "numeric",
+                    month: "long",
+                  })}
+                </p>
+              </div>
+              <div className="flex items-center gap-2">
+                <p className="text-lg font-bold">{Math.round(d.main.temp)}°</p>
+                {getIcon(d.weather[0].main, 30)}
+              </div>
+              <div className="flex gap-2 text-sm opacity-90">
+                <p>{Math.round(d.main.temp_min)}°</p>
+                <p>{Math.round(d.main.temp_max)}°</p>
+                <p>{Math.round(d.main.feels_like)}°</p>
+              </div>
             </div>
-            <div className="flex items-center gap-2">
-              <p className="text-lg font-bold">{Math.round(d.main.temp)}°</p>
-              {getIcon(d.weather[0].main)}
-            </div>
-            <div className="flex gap-2 text-sm opacity-90">
-              <p>{Math.round(d.main.temp_min)}°</p>
-              <p>{Math.round(d.main.temp_max)}°</p>
-              <p>{Math.round(d.main.feels_like)}°</p>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </section>
   );
